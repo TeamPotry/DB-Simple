8fb59f934c4bbbfa554965e14e3d2cbd048eea1a
Nopied◎ <iuy0223@naver.com>
2022-07-01 00:03:56 +0000

    Added vscode setting and update batch script for Codespace

---
+++ batch/update-all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# a batch file, used for codespace.
+# use this before compile at first time.
+
+cd /
+
+# delete stuff that already downloaded.
+sudo rm -rf ./sourcemod_lib ./sourcemod-latest-linux
+sudo mkdir ./sourcemod_lib && cd ./sourcemod_lid
+# download SourceMod
+sudo wget --input-file=http://sourcemod.net/smdrop/1.10/sourcemod-latest-linux
+sudo tar -xzf $(cat sourcemod-latest-linux)
+
+# delete unneccessry folders.
+sudo mv ./addons/sourcemod/scripting/* ./ 
+sudo rm -rf ./addons ./cfg
+
+# Setup Include (same as Github Action)
+# Nothing for now.
